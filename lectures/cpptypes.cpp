6e0d1663bda009bc9f9a5704da2ba95e59c675ba
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-09-01 15:51:46 -0600

    Thursday 0901

---
+++ lectures/cpptypes.cpp
@@ -0,0 +1,42 @@
+/*
+Name: Jeremy Bergen
+Date: 09/01/22
+
+Learning C++ Types!!!
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string fullName = "Jeremy Bergen";
+
+    return 0;
+}
+
+    // int numSeconds, secondsPerHour;
+    // int total;
+
+    // numSeconds = 12345;
+    // secondsPerHour = 3600;
+
+    // total = numSeconds % secondsPerHour;
+
+    // cout << numSeconds << " % " << secondsPerHour << " = " 
+    //      << total << endl;
+
+
+
+    // total = total + num2;
+
+    // cout << total-1 << " + " << num2 << " = " 
+    //      << total << endl;
+
+    // myChar = 'J';
+
+    // cout << "myChar is: " << myChar << endl;
+
+    // myChar = 'B';
+
+    // cout << "myChar is: " << myChar << endl;
