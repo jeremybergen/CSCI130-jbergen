74bfed94d3d1c2f1944a093024a095df2d0d69b2
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-09-06 15:27:32 -0600

    prompt for name

--- lectures/rectangle.cpp
+++ lectures/rectangle.cpp
@@ -5,5 +5,31 @@ Date: 09/06/22
 This program will prompt for 2 sides of a rectangle
 and calculate the area and perimeter
 
-
+step 1: prompt user for name
+step 2: greet user
+step 7: prompt for sides of a rectangle
+step 8: calculate area
+step 9: calculate the perimeter
+step 10: print area and perimeter
 */
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string fullName;
+
+    // step 1: prompt user for name
+    cout << "Please enter your full name: ";
+    getline(cin, fullName);
+    
+    // step 2: greet user
+    cout << "Hello " << fullName << endl;
+    
+    // step 7: prompt for sides of a rectangle
+    // step 8: calculate area
+    // step 9: calculate the perimeter
+    // step 10: print area and perimeter
+    return 0;
+}
