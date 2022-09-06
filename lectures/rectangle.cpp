617097f92a3f1acf4e574fee44a73f61a534c450
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-09-06 15:39:31 -0600

    final rectangle commit

--- lectures/rectangle.cpp
+++ lectures/rectangle.cpp
@@ -19,6 +19,8 @@ using namespace std;
 
 int main() {
     string fullName;
+    unsigned int side1, side2;
+    unsigned int area, perimeter;
 
     // step 1: prompt user for name
     cout << "Please enter your full name: ";
@@ -26,10 +28,32 @@ int main() {
     
     // step 2: greet user
     cout << "Hello " << fullName << endl;
-    
+
     // step 7: prompt for sides of a rectangle
+    cout << "Please enter side 1: ";
+    cin >> side1;
+
+    cout << "Please enter side 2: ";
+    cin >> side2;
+
+    // cout << "DEBUG: side1: " << side1 << endl;
+    // cout << "DEBUG: side2: " << side2 << endl;
+
     // step 8: calculate area
+    area = side1 * side2;
+
     // step 9: calculate the perimeter
+    perimeter = (2 * side1) + (2 * side2);
+
+    // cout << "DEBUG: area: " << area << endl;
+    // cout << "DEBUG: perimeter: " << perimeter << endl;
+
     // step 10: print area and perimeter
+    cout << "The rectangle with sides "
+         << side1 << " and " << side2
+         << " has an area of " << area
+         << " and a perimeter of " << perimeter << endl;
+
+    // cout << area << endl << perimeter << endl;
     return 0;
 }
