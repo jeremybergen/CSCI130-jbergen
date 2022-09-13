e1faecb4a969e4ff1e92a0d86b2799b2235acdce
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-09-13 15:49:46 -0600

    Tuesday 0913 Function Prototypes

---
+++ lectures/funcRectangle.cpp
@@ -0,0 +1,94 @@
+/*
+Name: Jeremy Bergen
+
+This is calculating a rectangle using functions!!!
+
+step 1: prompt user for name
+step 2: greet user
+step 7: prompt for sides of a rectangle
+step 8: calculate area
+step 9: calculate the perimeter
+step 10: print area and perimeter
+*/
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void greetUser(string firstName);
+void promptName();
+double rectangleSide();
+double calcArea(double s1, double s2);
+double calcPerim(double s1, double s2);
+void printVals(double area, double perim, double side1, double side2);
+
+int main() {
+    double side1, side2;
+    double area, perim;
+
+    promptName();
+
+    side1 = rectangleSide();
+    side2 = rectangleSide();
+
+    area = calcArea(side1, side2);
+
+    perim = calcPerim(side1, side2);
+
+    printVals(area, perim, side1, side2);
+
+    return 0;
+}
+
+//step 2
+void greetUser(string firstName) {
+    cout << "Hello " << firstName << endl;
+}
+
+//step 1
+void promptName() {
+    string name;
+
+    cout << "Please enter your name: ";
+    cin >> name;
+
+    greetUser(name);
+    // return name;
+}
+
+//step 7 prompt for the sides of a rectangle
+double rectangleSide() {
+    double asdf;
+
+    cout << "Please enter a side of the rectangle: ";
+    cin >> asdf;
+
+    return asdf;
+}
+
+// step 8: calculate area
+double calcArea(double s1, double s2) {
+    double area;
+
+    area = s1 * s2;
+
+    return area;
+}
+
+// step 9: calculate the perimeter
+double calcPerim(double s1, double s2) {
+    double perim;
+
+    perim = (2*s1) + (2*s2);
+
+    return perim;
+}
+
+// step 10: print area and perimeter
+void printVals(double area, double perim, double side1, double side2) {
+    cout << "The rectangle with sides "
+         << side1 << " and " << side2
+         << " has an area and perimeter of: " << endl
+         << "area: " << area << endl
+         << "perimeter: " << perim << endl;
+}
--- lectures/functions.cpp
+++ lectures/functions.cpp
@@ -8,59 +8,84 @@ C++ Functions...Cool...
 
 using namespace std;
 
-int addTwo() {
-    int num1;
-    
-    cout << "Please enter a number: ";
-    cin >> num1;
-
-    return num1 + 2;
-}
-
-
-
-void sayHello(string fName, string lName, int n1) {
-    cout << "Hello " << fName << " " << lName << endl;
-    // string name;
-
-    // cout << "Please enter your first name: ";
-    // cin >> name;
-
-    // cout << "Hello " << name << endl;
-    // cout << "Hello New World" << endl;
-    // cout << "Another line" << endl;
-}
-
-string getName() {
-    string firstName;
-
-    cout << "Please enter your first name: ";
-    cin >> firstName;
-
-    sayHello(firstName, "Bergen", 42);
-
-    return firstName;
-}
-
-int addNums(int n1, int n2) {
-    // cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
-    return n1 + n2;
-}
+void sayHello();
+void sayGoodbye();
+int addNums(int, int);
 
 int main() {
-    string yourName;
-    int total;
-    
-    yourName = getName();
-
-    sayHello(yourName, "Skywalker", 0);
-
-    total = addNums(42, 15);
-
-    cout << "Your total is: " << total << endl;
+    // sayGoodbye();
+    cout << "42 + 15 = " << addNums(42, 15) << endl;
 
     return 0;
 }
+
+int addNums(int n1, int n2) {
+    int total;
+
+    total = n1 + n2;
+
+    return total;
+}
+
+void sayHello() {
+    cout << "Hello" << endl;
+    sayGoodbye();
+}
+
+void sayGoodbye() {
+    cout << "Goodbye" << endl;
+    sayHello();
+}
+
+// int addTwo() {
+//     int num1;
+    
+//     cout << "Please enter a number: ";
+//     cin >> num1;
+
+//     return num1 + 2;
+// }
+
+
+
+// void sayHello(string fName, string lName, int n1) {
+//     cout << "Hello " << fName << " " << lName << endl;
+//     // string name;
+
+//     // cout << "Please enter your first name: ";
+//     // cin >> name;
+
+//     // cout << "Hello " << name << endl;
+//     // cout << "Hello New World" << endl;
+//     // cout << "Another line" << endl;
+// }
+
+// string getName() {
+//     string firstName;
+
+//     cout << "Please enter your first name: ";
+//     cin >> firstName;
+
+//     sayHello(firstName, "Bergen", 42);
+
+//     return firstName;
+// }
+
+// int addNums(int n1, int n2) {
+//     // cout << n1 << " + " << n2 << " = " << n1 + n2 << endl;
+//     return n1 + n2;
+// }
+
+    // string yourName;
+    // int total;
+    
+    // yourName = getName();
+
+    // sayHello(yourName, "Skywalker", 0);
+
+    // total = addNums(42, 15);
+
+    // cout << "Your total is: " << total << endl;
 
     // string firstName;
     // int myNum;
