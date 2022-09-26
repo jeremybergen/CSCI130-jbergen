516cf1c6e4a13957beaf66be727ae9926771642e
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-09-26 15:49:36 -0600

    Monday 0926

---
+++ lectures/looping.cpp
@@ -0,0 +1,114 @@
+/*
+Name: Jeremy
+
+all about the loops
+*/
+#include <iostream>
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    for (float i=0; i<1; i+=.1) {
+        cout << i << endl;
+    }
+    // string firstName = "JEREMY";
+
+    // for (int i=0; i<6; i++) {
+    //     cout << firstName[i] << endl;
+    // }
+
+    // cout << (int)'J' << endl;
+
+    // for (char ch : firstName) {
+    //     cout << (int)ch << endl;
+    //     cout << tolower(ch) << endl;
+    // }
+
+
+    // for (int i : {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}) {
+    //     cout << i << endl;
+    // }
+
+    
+    // for (int i=1, j=10; i<=10 && j>=1; i++, j--) {
+    //     cout << i << " + " << j << " = " << i+j << endl;
+    // }
+
+    // for ( ; ; ) {
+    //     int counter;
+    //     if (counter > 10) {
+    //         break;
+    //     }
+    //     cout << counter << endl;
+
+    //     counter++;
+    // }
+
+    // cout << "counter is: " << counter << endl;
+
+    // for(int i=0; i<=10; i++) {
+    //     cout << i << endl;
+    //     i++;
+    // }
+
+    // for(int i=0; i<=10; i++) {
+    //     if (i%2 == 0) {
+    //         cout << i << endl;
+    //     }
+    // }
+
+    // for(int i=0; i<=10; i+=2) {
+    //     cout << i << endl;
+    // }
+
+    // for(int i=0; i<=10; i++) {
+    //     if (i%2 == 1) {
+    //         continue;
+    //     }
+    //     cout << i << endl;
+    // }
+    // int side1;
+
+    // for( ; ; ) {
+    //     cout << "Please enter a positive side: ";
+    //     cin >> side1;
+
+    //     if (side1 > 0) {
+    //         break;
+    //     }
+    //     cout << "Your side must be greater than zero" << endl;
+    // }
+
+    // cout << "You entered: " << side1 << endl;
+
+    // for (int i=0; i >= 0; i++) {
+    //     cout << i << endl;
+    // }
+
+    // for (int i=0; i<=10; i++) {
+    //     // cout << i << "-Hello" << endl;
+    //     for (int j=10; j>=0; j--) {
+    //         cout << i*j << " ";
+    //     }
+    //     cout << endl;
+    // }
+    // cout << i << endl;
+
+    // cout << "1-Hello" << endl;
+    // cout << "2-Hello" << endl;
+    // cout << "3-Hello" << endl;
+    // cout << "4-Hello" << endl;
+    // cout << "5-Hello" << endl;
+
+    // int x = 42;
+
+    // // x -= 1
+    // // x = x - 1
+    // cout << --x << endl;
+    // cout << x << endl;
+
+    return 0;
+}
