dbb9a22e4fbedf69ad0f361c34e343c76cbe683e
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-10-11 15:50:51 -0600

    Tuesday 1011

--- lectures/arrays.cpp
+++ lectures/arrays.cpp
@@ -4,20 +4,69 @@ Name: Jeremy Bergen
 All About Arrays
 */
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
+using big_int = long long int;
+typedef long long int large_int;
+
 int main(int argc, char *argv[]) {
     // int nums[] = {42, 15, -9, 9000, -14323};
-    unsigned int arrSize;
+    // unsigned long arrSize = 4000000000/sizeof(int);
+    size_t arrSize = 5;
+    int *nums = new int[arrSize];
+    int *nums2 = new int[arrSize];
+
+    nums[0] = 42;
+
+    cout << "&nums[0]: " << &nums[0] << endl;
+    cout << "&nums2[0]: " << &nums2[0] << endl;
+    nums2 = nums;
+
+    cout << "&nums[0]: " << &nums[0] << endl;
+    cout << "&nums2[0]: " << &nums2[0] << endl;
+    // int *nums = new int[arrSize];
+
+    // big_int num1;
+    // large_int num1Again;
+    // long long int num1YetAgain;
+
+    // cout << sizeof(long long int) << endl;
+    // cout << sizeof(big_int) << endl;
+    // cout << sizeof(large_int) << endl;
+
+    // for (size_t i = 0; i <4000000000/sizeof(int); i++) {
+    //     nums[i] = i;
+    // }
+    // nums[999999] = 42;
+
+    // cout << *(nums + 999999) << endl;
+    // cout << nums[999999] << endl;
+    // int *ptr;
+
+    // for (size_t i = 0; i < arrSize; i++) {
+    //     cout << &nums[i] << endl;
+    //     // cout << "nums[" << i << "]: " << nums[i] << endl;
+    // }
+
+    // ptr = &nums[0];
+
+    // int ptrSize = sizeof(nums)/sizeof(int);
+    // for (size_t i = 0; i < ptrSize; i++) {
+    //     cout << *ptr << endl;
+    //     ptr++;
+    // }
+    // cout << "num1: " << num1 << endl;
+    // cout << "num2: " << num2 << endl;
 
 
-    cout << "Please enter a size of the array: ";
-    cin >> arrSize;
+    // cout << "Please enter a size of the array: ";
+    // cin >> arrSize;
     
-    int nums[arrSize];
+    // int nums[arrSize];
 
-    cout << sizeof(nums)/sizeof(int) << endl;
+    // cout << sizeof(nums)/sizeof(int) << endl;
 
     // for (unsigned int i = 0; i < arrSize; i++) {
     //     cout << "Please enter a number: ";
