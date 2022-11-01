38883a8ae9523cfe2d43d31a2a78627f564a5c28
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-11-01 15:49:42 -0600

    Tuesday 1101

--- lectures/fileio.cpp
+++ lectures/fileio.cpp
@@ -12,19 +12,50 @@ using namespace std;
 int main(int argc, char *argv[]) {
     ifstream fin;
     ofstream fout;
+    fstream fileStream;
     // string myString, myString2;
-    int myNumbers;
+    // int myNumbers;
 
-    fin.open("../labs/files/input.txt");
+    // fileStream.open("../labs/files/basic_fstream.txt", ios_base::in|ios_base::out);
+    
+    // cout << fileStream.tellg() << endl;
+    // fileStream.seekg(0, ios_base::end);
+    // cout << fileStream.tellg() << endl;
+    // fileStream.seekg(0);
+    // fileStream.seekg(19);
+    // fileStream.put('c');
+    // fileStream.put('c');
+    // fileStream.put('c');
+    // fileStream.put('c');
+    // fileStream.put('c');
+    // fileStream.put('c');
+    // char character = fileStream.get();
+    // cout << character << endl;
+    fin.open("../labs/files/vscode.exe", ios_base::ate|ios_base::binary);
     // fin.open("files\\input.txt");
     // fin.open("c:\\Users\\jbergen\\CSCI130-jbergen\\lectures\\files\\input.txt");
     // fin.open("/root/CSCI130-jbergen/lectures/files/input.txt");
-    fout.open("../labs/files/output.txt");
+    fout.open("../labs/files/vscode2.exe", ios_base::app|ios_base::binary);
+    // fout.open("../labs/files/output.txt", 1);
     // fin.open("/root/CSCI130-jbergen/lectures/files/output.txt");
 
-    while(fin >> myNumbers) {
-        fout << myNumbers << endl;
-    }
+    cout << fin.tellg() << endl;
+    size_t bufSize = fin.tellg();
+    char *buffer = new char[bufSize];
+    fin.seekg(0);
+    fin.read(buffer, bufSize);
+    // for(size_t i = 0; i < bufSize; i++) {
+    //     buffer[i] = fin.get();
+    // }
+
+    fout.write(buffer, bufSize);
+    // for(size_t i = 0; i < bufSize; i++) {
+    //     fout << buffer[i];
+    // }
+
+    // while(fin >> myNumbers) {
+    //     fout << myNumbers << endl;
+    // }
 
     // fin >> myString >> myString2;
     // getline(fin, myString);
@@ -40,6 +71,8 @@ int main(int argc, char *argv[]) {
     //     cout << myString << endl;
     // }
 
+    // fileStream.close();
+    delete[] buffer;
     fin.close();
     fout.close();
 
