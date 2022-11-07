1cac546c0e3cc3256a6f8bfebfc03e791db86eec
Jeremy Bergen <jbergen@coloradomesa.edu>
2022-11-07 15:50:12 -0700

    Monday 1107

--- lectures/structures.cpp
+++ lectures/structures.cpp
@@ -6,6 +6,7 @@ Structures
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <sstream>
 
 using namespace std;
 
@@ -15,29 +16,119 @@ struct someStruct {
     string someChar;
 };
 
+template <class T1, class T2>
 struct Points {
-    int x;
-    int y;
+    T1 x;
+    T2 y;
 };
 
+// void promptXY(Points&);
+Points promptXY();
+string printPoint(Points);
+double calcDistance(Points&, Points&);
+
 int main(int argc, char *argv[]) {
-    float distance;
+    double distance;
     Points point1;
     Points point2;
-    Points myPoints[2];
 
-    myPoints[1] = point2;
+    // promptXY(point1);
+    // promptXY(point2);
+    point1 = promptXY();
+    point2 = point1;
+    // point2 = promptXY();
 
-    point1.x = 0;
-    point1.y = 0;
-    myPoints[0] = point1;
+    // printPoint(point1);
+    // printPoint(point2);
 
-    myPoints[1].x = 1;
-    myPoints[1].y = 1;
+    distance = calcDistance(point1, point2);
+    // cout << "Distance: " << distance << endl;
+    cout << "The distance between points " 
+         << printPoint(point1) << " and " 
+         << printPoint(point2) 
+         << " is " << distance << "." << endl;
 
-    distance = sqrt(pow((myPoints[0].x - myPoints[1].x), 2) + pow((myPoints[0].y - myPoints[1].y), 2));
+    // delete point1;
+    // delete point2;
 
-    cout << "The distance is: " << distance << endl;
+    return 0;
+}
+
+string printPoint(Points point) {
+    char outputString[16];
+
+    string printoutString;
+
+    printoutString = "(" + to_string(point.x) + ", " + to_string(point.y) + ")";
+    sprintf(outputString, "(%d, %d)", point.x, point.y);
+    cout << "DEBUG: outputString: " << outputString << endl;
+
+
+    // stringstream ss;
+
+    // ss << "(" 
+    //      << point.x << ", " 
+    //      << point.y << ")";
+
+    // // ss >> printoutString;
+    // getline(ss, printoutString);
+
+    // cout << "DEBUG: printoutString: " << printoutString << endl;
+
+    return printoutString;
+}
+
+double calcDistance(Points &point1, Points &point2) {
+    double distance;
+    distance = sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2));
+    return distance;
+}
+
+// void promptXY(Points &newPoint) {
+Points promptXY() {
+    Points newPoint;
+    char ch;
+    cout << "Please enter a coordinate in the form (x, y): ";
+    cin >> ch >> newPoint.x >> ch >> newPoint.y >> ch;
+    // cout << "DEBUG: newPoint.x: " << newPoint.x << endl;
+    // cout << "DEBUG: newPoint.y: " << newPoint.y << endl;
+    return newPoint;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Points point1;
+    // Points point2;
+    // Points myPoints[2];
+
+    // myPoints[1] = point2;
+
+    // point1.x = 0;
+    // point1.y = 0;
+    // myPoints[0] = point1;
+
+    // myPoints[1].x = 1;
+    // myPoints[1].y = 1;
+
+    // distance = sqrt(pow((myPoints[0].x - myPoints[1].x), 2) + pow((myPoints[0].y - myPoints[1].y), 2));
+
+    // cout << "The distance is: " << distance << endl;
 
     // someStruct bob;
     // someStruct *heapBob = new someStruct;
@@ -59,6 +150,3 @@ int main(int argc, char *argv[]) {
 
     // // cout << bob.someChar << endl;
     // delete heapBob;
-
-    return 0;
-}
