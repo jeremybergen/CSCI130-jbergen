4f3131f2b118898f08b355f82f827776f01915c3
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-01-25 09:48:01 -0700

    Our first hello world program

---
+++ hello.cpp
@@ -0,0 +1,18 @@
+/*
+Name: Jeremy Bergen
+
+First Hello World Program
+This will get ignored by the compiler
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // This will print out hello world to the console
+    // This is a second line
+    cout << "Hello World again" << endl;
+    cout << "This is a second hello" << endl;
+    return 0; // This exits the program with value 0
+}
