ffe6c80ec24fad134f5ce7ebf7fbc15471dd102f
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-01-30 09:52:30 -0700

    tuesday 0130

---
+++ lecture/variables.cpp
@@ -0,0 +1,19 @@
+/*
+Name: Jeremy Bergen
+
+All about the variables
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number1;
+
+    number1 = 42;
+
+    cout << number1 << endl;
+    //cout << "Hello World" << endl;
+    return 0;
+}
