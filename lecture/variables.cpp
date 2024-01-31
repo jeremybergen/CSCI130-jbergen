4d1dec7127c67663218ecfdfd16a69f6e37bd04c
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-01-30 20:55:25 -0700

    wednesday 0131

--- lecture/variables.cpp
+++ lecture/variables.cpp
@@ -11,9 +11,77 @@ int main()
 {
     int number1;
 
-    number1 = 42;
+    number1 = 2147483647;
 
     cout << number1 << endl;
+
+    number1 = number1 - 1;
+
+    cout << number1 << endl;
+
+    number1 -= 1;
+
+    cout << number1 << endl;
+
+    number1--;
+
+    cout << number1 << endl;
+
+    --number1;
+
+    cout << number1 << endl;
+    // cout << sizeof(string) << endl;
+    // string str1;
+    // string str2;
+    // char ch1;
+
+    // str1 = "J";
+    // ch1 = 'J';
+    // // int sum;
+
+    // // number1 = 42;
+    // // number2 = 15;
+
+    // // sum = number1 + number2;
+
+    // cout << "Please enter number 1: ";
+    // cin >> number1;
+
+    // cout << "Please enter number 2: ";
+    // cin >> number2;
+
+    // cout << number1 << " + " << number2 << " = " << number1 + number2 << endl;
+
+    // double number1;
+
+    // number1 = 72.42;
+
+    // cout << number1 << endl;
+    // char someLetter;
+
+    // someLetter = 42;
+
+    // cout << someLetter << endl;
+    // int number1;
+
+    // number1 = 'J';
+
+    // cout << number1 << endl;
+
+    // cout << sizeof(bool) << endl;
+    // long number1;
+    // long number2;
+    // long number3;
+    // long number4;
+
+    // cout << &number1 << endl;
+    // cout << &number2 << endl;
+    // cout << &number3 << endl;
+    // cout << &number4 << endl;
+
+    // number1 = 42;
+
+    // cout << number1 << endl;
     //cout << "Hello World" << endl;
     return 0;
 }
