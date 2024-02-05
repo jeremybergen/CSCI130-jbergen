89d5c9cebbf01037f697a700d8c10799bc9186ac
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-02-05 15:06:06 -0700

    tuesday 0206

--- lecture/libraries.cpp
+++ lecture/libraries.cpp
@@ -10,6 +10,24 @@ using namespace std;
 
 int main()
 {
+    // int numSeconds;
+    // int origSeconds;
+    // int numMinutes;
+    // int numHours;
+
+    // cout << "Enter number of seconds: ";
+    // cin >> numSeconds;
+    // origSeconds = numSeconds;
+
+    // numHours = numSeconds/3600;
+    // numMinutes = (numSeconds/60) - (numHours * 60);
+    // numSeconds = numSeconds%60;
+
+    // cout << "The hours:minutes:seconds given " << origSeconds << " seconds is: "
+    //      << numHours << ":" << numMinutes << ":" << numSeconds << endl;
+    // numMinutes = (numMinutes - (numHours * 60)) % 60;
+
+    // cout << 127%60 << endl;
     // float result = ((180/3.14)*cos(42))+360;
     // cout << result << endl;
     // cout << (180/M_SQRT1_2)*cos(42) << endl;
