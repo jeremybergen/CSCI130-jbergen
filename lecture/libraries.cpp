988b549f713533d455cd964299d0a80514021a9b
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-02-05 09:51:51 -0700

    monday 0205

---
+++ lecture/libraries.cpp
@@ -0,0 +1,21 @@
+/*
+Name: Jeremy Bergen
+
+Libraries
+*/
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    // float result = ((180/3.14)*cos(42))+360;
+    // cout << result << endl;
+    // cout << (180/M_SQRT1_2)*cos(42) << endl;
+    // int n1, n2;
+    // n1 = 2;
+    // n2 = 8;
+    // cout << (n1^n2) << endl;
+    return 0;
+}
