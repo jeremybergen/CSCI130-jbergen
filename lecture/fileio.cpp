a4b486bba839e878188c0461aed7ccb9dddc3cb1
Jeremy Bergen <jbergen@coloradomesa.edu>
2024-04-04 09:51:45 -0600

    thursday 0404

--- lecture/fileio.cpp
+++ lecture/fileio.cpp
@@ -10,28 +10,62 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    string numbers[5];
+    // string fName, lName, title, color;
+    string lines[3][4];
+    string inputLine;
     ifstream fin;
-    ofstream fout;
 
-    fin.open("/home/jbergen/projects/CSCI130-jbergen/Assignments/HW1/files/input.txt");
-    //fin.open("c:\\Users\\jbergen\\projects\\CSCI130-jbergen\\Assignments\\HW1\\files\\input.txt");
-    fout.open("files/output.txt", ios_base::out|ios_base::app);
+    fin.open("files/input.csv");
 
-    for(int i = 0; i < 5; i++)
+    int rowNumber = 0;
+    int colNumber = 0;
+    while(getline(fin, inputLine))
     {
-        // fin >> numbers[i];
-        getline(fin, numbers[i]);
-    }
-    // fin >> numbers[0] >> numbers[1] >> numbers[2] >> numbers[3] >> numbers[4];
-
-    for(int i = 0; i < 5; i++)
-    {
-        fout << "numbers[" << i << "]: " 
-             << numbers[i] << endl;
+        cout << "DEBUG: " << inputLine << endl;
+        // Jeremy,Bergen,Assistant Professor of Computer Science,Red
+        cout << "DEBUG: : " << inputLine.find(',') << endl;
+        lines[rowNumber][colNumber] = 
+        rowNumber++;
     }
 
-    fout.close();
     fin.close();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+    // string numbers[5];
+    // ifstream fin;
+    // ofstream fout;
+
+    // fin.open("files/input.txt", ios_base::ate);
+    // // cout << "tellg: " << fin.tellg() << endl;
+    // // char buffer[fin.tellg()];
+    // // fin.seekg(0);
+    // // return 0;
+
+    // //fin.open("c:\\Users\\jbergen\\projects\\CSCI130-jbergen\\Assignments\\HW1\\files\\input.txt");
+    // fout.open("files/output.txt", ios_base::out|ios_base::app);
+
+    // for(int i = 0; i < 5; i++)
+    // {
+    //     // fin >> numbers[i];
+    //     getline(fin, numbers[i]);
+    // }
+    // // fin >> numbers[0] >> numbers[1] >> numbers[2] >> numbers[3] >> numbers[4];
+
+    // for(int i = 0; i < 5; i++)
+    // {
+    //     fout << "numbers[" << i << "]: " 
+    //          << numbers[i] << endl;
+    // }
+
+    // fout.close();
+    // fin.close();
